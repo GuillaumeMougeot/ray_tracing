59c8ad79259df639eb4d52fbf59684c1517ae5c9
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2017-12-16 00:59:52 +0800

    is intersect method implementation

--- include/Sphere.hpp
+++ include/Sphere.hpp
@@ -13,16 +13,20 @@ public:
     Eigen::Vector3d pos,
     Eigen::Quaterniond rot,
     Eigen::Vector3d center,
-    Eigen::Vector3d radius,
+    double radius,
     Eigen::Vector3d mat = Material::Default()
-  ): PhysicalObject(pos, rot, mat), m_radius(radius), m_center(center) {}
+  ): PhysicalObject(pos, rot, mat), m_radius(radius), m_center(center), m_d1(0), m_discriminant(0) {}
 
   bool IsIntersect(Ray* ray);
   Eigen::Vector3d Intersect(Ray* ray);
 
 private:
-  Eigen::Vector3d m_radius;
+  double m_radius;
   Eigen::Vector3d m_center;
+
+  // Useful variables for computing intersection point
+  double m_d1;
+  double m_discriminant;
 };
 
 #endif
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -3,6 +3,9 @@
 // https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 bool Sphere::IsIntersect(Ray* ray)
 {
-  bool result;
-  
+  Eigen::Vector3d d = ray->getOrigin() - m_center
+  m_d1 = ray->getDirection().dot(d);
+  m_discriminant = m_d1*m_d1 - d.squaredNorm() + m_radius*m_radius;
+
+  return(m_discriminant >= 0)
 }
