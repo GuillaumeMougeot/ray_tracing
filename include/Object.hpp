35aa7d52ec87ee13f11632798e6499e631e9b173
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2017-12-15 15:15:58 +0800

    add accessors.

--- include/Object.hpp
+++ include/Object.hpp
@@ -9,9 +9,13 @@ class Object
 public:
   //Constructors
   Object();
-  // Object(Eigen::Vector3d pos);
-  // Object(Eigen::Quaternion rot);
-  // Object(Eigen::Vector3d pos, Eigen::Quaternion rot);
+  Object(Eigen::Vector3d pos);
+  Object(Eigen::Quaterniond rot);
+  Object(Eigen::Vector3d pos, Eigen::Quaterniond rot);
+
+  //Accessors
+  Eigen::Vector3d getPos() {return m_pos;}
+  Eigen::Quaterniond getRot() {return m_rot;}
 
 protected:
   Eigen::Vector3d m_pos;
